fcf9af823f4de4af8ae6d59b3f342da6c57b0f58
NHQ SERVER ADMIN <nhq@NHQ.att.net>
2014-03-01 18:36:36 -0800

    added typed array support

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
     npm install sloop
 
 **loop over a selected portion of a buffer**
-
+**now supports Typed Arrays**
+```js
     var Sampler = require('./sloop')
 
     var buf = new Buffer('beepboopbaapboom')
     , start = 0 // buf index to start
-    , byteSize = 4 // size of each sample in bytes
-    , duration = 4 // length of loop in samples (not bytes)
+    , size = 4 // size of each sample by index count (bytes, for buffers;  indecies for TAs (usually 1))
+    , duration = 4 // length of the loop in the above size value
     ;
     
-    sample = Sampler(buf, start, duration, byteSize)
+    sample = Sampler(buf, start, duration, size)
     
     var t = setInterval(
       function(){
       console.log(sample().toString())
     }, 333)
+```
--- sloop.js
+++ sloop.js
@@ -1,13 +1,28 @@
 module.exports = function(buffer, start, duration, sampleLength){
+  var isBuffer = Buffer.isBuffer(buffer)
+  var sampler;
 
-  function sampler(){
+  function bufferSampler(){
     if(sampler.index >= sampler.duration) sampler.index = 0;
     sampler.index = sampler.start + sampler.index;
     var sample = sampler.buffer.slice(sampler.index, sampler.index + sampler.sampleLength);
     sampler.index += sampler.sampleLength;
     return sample
   }
-
+  
+  function typedArraySampler(){
+    if(sampler.index >= sampler.duration) sampler.index = 0;
+    sampler.index = sampler.start + sampler.index;
+    var sample = sampler.buffer.subarray(sampler.index, sampler.index + sampler.sampleLength);
+    sampler.index += sampler.sampleLength;
+    return sample
+  }
+  
+  if(!isBuffer){
+    sampler = typedArraySampler
+  }
+  else sampler = bufferSampler 
+  
   sampler.start = start;
   sampler.duration = duration * sampleLength;
   sampler.sampleLength = sampleLength
